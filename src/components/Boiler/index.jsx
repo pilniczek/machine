a7d736652d7cc4e0e7ad4a26d1206f570b5dec00
Tomáš Pilnaj <tomas.pilnaj@gmail.com>
2018-01-22 08:42:42 +0100

    convert Boiler elements coordinates from absolute values to relative values

--- src/components/Boiler/index.jsx
+++ src/components/Boiler/index.jsx
@@ -16,29 +16,30 @@ const Boiler = ({x,y}) => (
         -15,-15 -15,0 0,-110 15,0 15,-15
         0,140 -15,-15 0,-110`}
     />
-    <rect className={css(s.creme)} x={50} y={320} width="90" height="105" />
-    <SideScrew x={60} y={315} className={css(s.creme)} />
-    <SideScrew x={130} y={315} className={css(s.creme)} />
-    <SideScrew x={95} y={315} className={css(s.creme)} />
-    <Screw x={60} y={340} className={css(s.creme)} />
-    <Screw x={130} y={340} className={css(s.creme)} />
-    <Screw x={60} y={380} className={css(s.creme)} />
-    <Screw x={130} y={380} className={css(s.creme)} />
-    <Screw x={60} y={415} className={css(s.creme)} />
-    <Screw x={130} y={415} className={css(s.creme)} />
-    <Screw x={95} y={415} className={css(s.creme)} />
-    <circle className={css(s.creme)} cx={95} cy={360} r="24" />
-    <circle className={css(s.sky)} cx={95} cy={360} r="20" />
-    <rect className={css(s.creme)} x={20} y={460} width="90" height="60" />
-    <SideScrew x={30} y={520} className={css(s.creme)} />
-    <SideScrew x={65} y={520} className={css(s.creme)} />
-    <SideScrew x={100} y={520} className={css(s.creme)} />
-    <SideScrew x={20} y={500} transform={"rotate(90)"} style={{transformOrigin: 'center top'}} />
-    <SideScrew x={20} y={470} transform={"rotate(90)"} style={{transformOrigin: 'center top'}} />
-    <Screw x={30} y={470} className={css(s.creme)} />
-    <Screw x={65} y={470} className={css(s.creme)} />
-    <Screw x={100} y={470} className={css(s.creme)} />
-    <Screw x={100} y={500} className={css(s.creme)} />
+
+    <rect className={css(s.creme)} x={x-130} y={y-30} width="90" height="105" />
+    <SideScrew x={x-115} y={y-35} className={css(s.creme)} />
+    <SideScrew x={x-85} y={y-35} classsName={css(s.creme)} />
+    <SideScrew x={x-55} y={y-35} className={css(s.creme)} />
+    <Screw x={x-115} y={y-10} className={css(s.creme)} />
+    <Screw x={x-55} y={y-10} className={css(s.creme)} />
+    <Screw x={x-115} y={y+25} className={css(s.creme)} />
+    <Screw x={x-55} y={y+25} className={css(s.creme)} />
+    <Screw x={x-115} y={y+60} className={css(s.creme)} />
+    <Screw x={x-55} y={y+60} className={css(s.creme)} />
+    <Screw x={x-85} y={y+60} className={css(s.creme)} />
+    <circle className={css(s.creme)} cx={x-85} cy={y+10} r="24" />
+    <circle className={css(s.sky)} cx={x-85} cy={y+10} r="20" />
+    <rect className={css(s.creme)} x={x-160} y={y+110} width="90" height="60" />
+    <SideScrew x={x-150} y={y+170} className={css(s.creme)} />
+    <SideScrew x={x-115} y={y+170} className={css(s.creme)} />
+    <SideScrew x={x-80} y={y+170} className={css(s.creme)} />
+    <SideScrew x={x-160} y={y+150} transform={"rotate(90)"} style={{transformOrigin: 'center top'}} />
+    <SideScrew x={x-160} y={y+120} transform={"rotate(90)"} style={{transformOrigin: 'center top'}} />
+    <Screw x={x-150} y={y+120} className={css(s.creme)} />
+    <Screw x={x-115} y={y+120} className={css(s.creme)} />
+    <Screw x={x-80} y={y+120} className={css(s.creme)} />
+    <Screw x={x-80} y={y+150} className={css(s.creme)} />
     {
       //<PointerGroup x={x+185} y={y+430}scale(.8)" />
     }
